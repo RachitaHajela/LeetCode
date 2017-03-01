7334b3c7494422f3724875f58ec46bad54b44812
Rachita Hajela <rachitahajela.25@gmail.com>
2017-03-01 16:00:19 -0500

    intersection of two LL

--- src/com/edu/ListNode.java
+++ src/com/edu/ListNode.java
@@ -4,7 +4,11 @@ package com.edu;
  * Created by RHAJELA on 7/28/2016.
  */
 public class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-  }
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
