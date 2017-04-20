50d89604a2a43f73e1f91935b77fbcbd52e02d55
RachitaHajela <rachitahajela.25@gmail.com>
2017-04-19 21:02:17 -0400

    another problem

---
+++ src/com/edu/BinaryTreePaths.java
@@ -0,0 +1,16 @@
+package com.edu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Rachita on 3/21/2017.
+ */
+public class BinaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String>  sol = new ArrayList<String>();
+
+        return sol;
+    }
+}
