2b23179f860e6a27b8a59e09daa411e6e53675fb
RachitaHajela <rachitahajela.25@gmail.com>
2017-08-09 22:44:00 -0400

    9 aug

--- src/com/edu/LongestAbsoluteFilePath.java
+++ src/com/edu/LongestAbsoluteFilePath.java
@@ -9,31 +9,49 @@ public class LongestAbsoluteFilePath {
 
     public static int lengthLongestPath(String input) {
         String[] splitDir = input.split("\n");
-        HashMap<Integer,Integer> charCount = new HashMap<>();
-        int level = 0;
+        HashMap<Integer, Integer> charCount = new HashMap<>();
         int maxLength = 0;
-        if(splitDir[0].length() > 0) {
-            charCount.put(level,splitDir[0].length());
-        }
+        charCount.put(0,0);
+        for(String s : splitDir) {
+            int level = s.lastIndexOf('\t') + 1;
+            System.out.println("level: "+ level);
+            int len = s.length() - level;
+            if (s.contains(".")) {
+                maxLength = Math.max(maxLength, charCount.get(level) + len);
+            } else {
+                charCount.put(level + 1, charCount.get(level) + len + 1);
+            }
 
-        for(int i = 1; i< splitDir.length; i++) {
+        }
+        return maxLength;
+       /* for (int i = 1; i < splitDir.length; i++) {
             String[] splitTab = splitDir[i].split("\t");
+            if(splitTab.length ==1) {
+                splitTab = splitDir[i].split("  ");
+            }
+            System.out.println("splitTab Len: "+splitTab.length);
             level = splitTab.length - 1;
             String word = splitTab[level];
-            System.out.println("each word: "+ word);
-            String[] name = word.split(".");
-            System.out.println("name length : "+ name.length);
-            if(name.length == 1) {
-                charCount.put(level,charCount.get(level-1)+name[0].length());
-                //System.out.println("level count : "+ charCount.get(level));
+            System.out.println("each word:" + word);
+
+            boolean isFile = false;
+            int count = 0;
+
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) == '.') {
+                    isFile = true;
+                }
+                count++;
             }
-            else {
-                //maxLength = Math.max(maxLength,charCount.get(level-1)+ name[0].length()+ name[1].length());
+            if (isFile) {
+                maxLength = Math.max(maxLength, charCount.get(level - 1) + count);
+                System.out.println("Maxlength: " + maxLength);
+            } else {
+                charCount.put(level, charCount.get(level - 1) + count+1);
             }
-            //System.out.println(splitDir[i] + "tab count : "+ splitTab.length);
         }
 
-        return maxLength;
+        return maxLength;*/
     }
 
     public static void main(String[] args) {
