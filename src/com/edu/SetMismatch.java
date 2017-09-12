16c07a027cc6ba3ff263042d636271a84e8e418d
Rachita Hajela <rachitahajela.25@gmail.com>
2017-09-12 15:37:52 -0400

    12 sep

--- src/com/edu/SetMismatch.java
+++ src/com/edu/SetMismatch.java
@@ -8,12 +8,36 @@ import java.util.HashSet;
 public class SetMismatch {
 
     public int[] findErrorNums(int[] nums) {
+
+        /* using this instead of set reduces time a lot!!
+        int[] arr = new int[nums.length + 1];
+        int dup = -1, missing = 1;
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]] += 1;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 0)
+                missing = i;
+            else if (arr[i] == 2)
+                dup = i;
+        }
+        return new int[]{dup, missing};*/
+
+
         int[] result = new int[2];
         HashSet<Integer> set = new HashSet<>();
         int n = nums.length;
 
         for(int i = 0; i < n; i++) {
-            
+            if(!set.add(nums[i])) {
+                result[0] = nums[i];
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(!set.remove(i)) {
+                result[1] = i;
+            }
         }
         return result;
     }
