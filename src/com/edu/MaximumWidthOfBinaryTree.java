3f2d2d5583ff92fa2e1f0274f2de4ebbff6515d4
RachitaHajela <rachitahajela.25@gmail.com>
2017-10-05 22:46:58 -0400

    oct 5

--- src/com/edu/MaximumWidthOfBinaryTree.java
+++ src/com/edu/MaximumWidthOfBinaryTree.java
@@ -10,25 +10,45 @@ import java.util.List;
 public class MaximumWidthOfBinaryTree {
 
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null || root.left == null || root.right == null) return 0;
+        if (root == null) return 0;
 
         ArrayList<TreeNode> queue = new ArrayList<>();
-        queue.add(root.left);
-        queue.add(root.right);
+        queue.add(root);
         int maxDistance = 0;
 
-        while(!queue.isEmpty()) {
-            int size = queue.size();
+        while (!queue.isEmpty()) {
+            int start = 0;
+            int end = queue.size() - 1;
+            int numOfElements = queue.size();
 
-            while(size!= 0) {
-                
+            while(start <= end && queue.get(start) == null) {
+                start++;
+            }
+            while(end >= 0 && queue.get(end) == null) {
+                end--;
+            }
 
+            int count = 0;
+            ArrayList<TreeNode> temp = new ArrayList<>();
+            for(int i = start; i<= end; i++) {
+                count++;
 
+                if(queue.get(i) == null) {
+                    temp.add(null);
+                    temp.add(null);
+                }
+                else {
+                    temp.add(queue.get(i).left);
+                    temp.add(queue.get(i).right);
+                }
 
             }
+            maxDistance = Math.max(maxDistance,count);
+
+           queue = temp;
         }
+        return maxDistance;
 
     }
-
 
 }
